a20824f7e7c6a576fd99d709953e561cb6361a85
GAURAV singh <gs1197418@gmail.com>
2026-06-24 21:39:52 +0530

    performance: add body size limit to express.json()

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 job.start();
-app.use(express.json())
+app.use(express.json({ limit: '10kb' }))
 app.use(cors())
 
 app.use('/api/v1',heathCheck)
